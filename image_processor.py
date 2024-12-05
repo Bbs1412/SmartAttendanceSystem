dc0038c58859ed96f76778201b14322d932d0177
Bhushan Songire <bhushanbsongire@gmail.com>
2024-12-05 19:16:50 +0530

    Refactor image_processor.py to support batch image processing with dynamic timestamps and folder organization

--- image_processor.py
+++ image_processor.py
@@ -1,29 +1,90 @@
 import os
 import base64
 from datetime import datetime
+from dotenv import load_dotenv
+from typing import Union, List
 
-# Inputs
-base64_string = input("Enter the Base64 string: ")
-timestamp = input("Enter timestamp (dd/mm/yyyy, hh:mm:ss AM/PM): ")
 
-# Folder and file handling
-curr_stamp = datetime.now().strftime("%Y-%m-%d_%Hh%Mm%Ss")
-output_folder = os.path.join("uploads", curr_stamp)
-os.makedirs(output_folder, exist_ok=True)
+load_dotenv()
+static_url = os.environ.get('static_url')
+upload_folder = os.environ.get("upload_folder")
 
-# Extract file extension dynamically
-try:
-    extension = base64_string.split(",")[0].split("/")[1].split(";")[0]
-    image_data = base64.b64decode(base64_string.split(",")[1])
 
-    # Generate filename from timestamp
-    dt_timestamp = datetime.strptime(timestamp, "%d/%m/%Y, %I:%M:%S %p")
+def get_py_stamp(js_timestamp: str):
+    """ Converts js timestamp to python timestamp
+
+    Args:
+        js_timestamp (str): JS timestamp in format: "dd/mm/yyyy, hh:mm:ss AM/PM"
+
+    Returns:
+        str: Python timestamp in format: "yyyy-mm-dd_hh-mm-ss"
+    """
+
+    dt_timestamp = datetime.strptime(js_timestamp, "%d/%m/%Y, %I:%M:%S %p")
     py_timestamp = dt_timestamp.strftime("%Y-%m-%d_%Hh%Mm%Ss")
-    output_file = f"{py_timestamp}.{extension}"
+    return py_timestamp
 
-    output_path = os.path.join(output_folder, output_file)
-    with open(output_path, "wb") as f:
-        f.write(image_data)
-    print(f"Image saved successfully at: {output_path}")
-except Exception as e:
-    print(f"Failed to process the image: {e}")
+
+def process_image(timestamps: Union[list, str], base64s: Union[list, str]):
+    """
+    Takes js timestamps and base64s
+    Converts into py stamps, and also, saves the images
+    Returns modified js stamps with _1, _2 etc. as well
+
+    Args:
+        timestamps (Union[list, str]): List of timestamps in format: "dd/mm/yyyy, hh:mm:ss AM/PM"
+        base64s (Union[list, str]): List of base64 strings
+
+    Returns:
+        List: List of file names
+        List: List of python timestamps
+        List: List of modified js timestamps
+    """
+
+    curr_stamp = datetime.now()
+    py_time_stamps = []
+    js_modified_time_stamps = []
+    file_names = []
+
+    # Issues is that, when multiple frames under same second are passed, our naming scheme does not support that
+    # Means, the same name is returned by function and only one image is over-written again n again with that PARTICULAR name.
+    # So keeping the last saved name in memory to add some _1, _2 after the repeated names.
+    last_saved = ""
+    same_name_count = 0
+
+    # with open("formData.json", 'w') as f:
+    #     f.write(json.dumps({'timestamps': timestamps, 'bases': base64s}))
+
+    for timestamp, base64_str in zip(timestamps, base64s):
+        # get the extension from: "data:image/jpeg;base64,full_base64_string"
+        extension = base64_str.split(',')[0].split('/')[1].split(';')[0]
+        # remove that part: "data:image/jpeg;base64"
+        image_data = base64.b64decode(base64_str.split(',')[1])
+
+        # Uploads/curr_timestamp_folder/all_images_in_that_session
+        subfolder = f'{curr_stamp.strftime("%Y-%m-%d_%Hh%Mm%Ss")}'
+        folder = os.path.join(static_url, upload_folder, subfolder)
+        os.makedirs(folder, exist_ok=True)
+
+        file_base_name = get_py_stamp(timestamp)
+
+        if file_base_name == last_saved:
+            same_name_count += 1
+            file_base_name += f'_{same_name_count}'
+        else:
+            last_saved = file_base_name
+            same_name_count = 0
+
+        file_name = f'{file_base_name}.{extension}'
+        file_path = os.path.join(folder, file_name)
+
+        file_names.append(file_path)
+        py_time_stamps.append(file_base_name)
+        js_modified_time_stamps.append(f"{timestamp}, {same_name_count}")
+
+        with open(file_path, 'wb') as f:
+            f.write(image_data)
+
+        # print(f'Saved image `{file_name}` successfully...')
+    return file_names, py_time_stamps, js_modified_time_stamps
+# process_image(timestamps, )
