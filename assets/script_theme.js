5afb21b4205db94efe78cf6b5f1efd8889daa176
Bhushan Songire <bhushanbsongire@gmail.com>
2025-02-11 05:07:31 +0530

    Add console logging for theme switch activation

--- assets/script_theme.js
+++ assets/script_theme.js
@@ -7,9 +7,11 @@ const themeSwitch = document.getElementById('switch-button');
 themeSwitch.addEventListener('change', () => {
     if (themeSwitch.checked) {
         document.documentElement.setAttribute('data-theme', 'light');
-        // document.getElementById('placeholderImage2').src = '/assets/camera_light.jpg';
+        // document.getElementById('placeholderImage').src = '/assets/camera_light.jpg';
+        console.info('JS: Light theme activated');
     } else {
         document.documentElement.removeAttribute('data-theme');
-        // document.getElementById('placeholderImage2').src = '/assets/camera2.jpg';
+        // document.getElementById('placeholderImage').src = '/assets/camera2.jpg';
+        console.info('JS: Dark theme activated');
     }
 });
